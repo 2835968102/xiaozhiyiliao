4c0062bf62f9d2659a8e780420bc23615acf509a
PMLLLLL <pengmaolin@proton.me>
2025-08-03 14:38:04 +0800

    配置依赖 访问 http://localhost:8080/doc.html 查看程序成功

---
+++ src/main/java/com/atguigu/java/ai/langchain4j/XiaozhiApp.java
@@ -0,0 +1,18 @@
+/**
+ * ClassName: XiaozhiApp
+ * Package: PACKAGE_NAME
+ * Description:
+ *
+ * @Author PML
+ * @Create 2025/8/3 14:32
+ * @Version 1.0
+ */
+package com.atguigu.java.ai.langchain4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class XiaozhiApp {
+    public static void main(String[] args) {
+        SpringApplication.run(XiaozhiApp.class, args);
+    }
+}
