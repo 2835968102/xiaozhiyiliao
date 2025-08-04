61b5f7dbd4d8a5d96020c4ec6adb115dee771171
PMLLLLL <pengmaolin@proton.me>
2025-08-04 15:44:50 +0800

    增加其他模型测试例程 ollama qwen

--- src/main/java/com/java/ai/langchain4j/XiaozhiApp.java
+++ src/main/java/com/java/ai/langchain4j/XiaozhiApp.java
@@ -1,3 +1,4 @@
+package com.java.ai.langchain4j;
 /**
  * ClassName: XiaozhiApp
  * Package: PACKAGE_NAME
@@ -7,9 +8,10 @@
  * @Create 2025/8/3 14:32
  * @Version 1.0
  */
-package com.atguigu.java.ai.langchain4j;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class XiaozhiApp {
     public static void main(String[] args) {
