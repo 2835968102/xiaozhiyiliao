2e44fec63dd0d6f8148826d4a437e2a4dc34a024
PMLLLLL <pengmaolin@proton.me>
2025-08-05 16:15:43 +0800

    会话持久化

--- src/main/java/com/java/ai/langchain4j/bean/ChatMessages.java
+++ src/main/java/com/java/ai/langchain4j/bean/ChatMessages.java
@@ -25,7 +25,10 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class ChatMessages {
     //唯一标识， 映射到 MongoDB 文档的 _id 字段
     @Id
-    private ObjectId messageId;
+    private ObjectId Id;
+
+    private int messageId;
+
     //private Long messageId;
     private String content; //存储当前聊天记录列表的json字符串
 }
